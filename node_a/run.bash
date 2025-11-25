e4f469e344cf023c7d23656f5d7398621016625a
Bradley Widjaja <bradleywidjaja507@gmail.com>
2025-11-25 11:01:51 +0700

    add nodes and programs

---
+++ node_a/run.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 peer_node.py \
+    --name A \
+    --listen 192.168.122.10 5000 \
+    --peers A@192.168.122.10:5000 B@192.168.122.11:5001 D@192.168.122.13:5002 \
+    --logger 192.168.122.12 9999 \
+    --offset-ms 600 \
+    --initiate-broadcast --msg "Hello from A"
